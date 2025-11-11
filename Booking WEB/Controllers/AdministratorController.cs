e49cd7c3403b083a124d4bc471cec023b8e314d0
Dyanavio <dimant2018@gmail.com>
2025-11-11 10:50:52 +0200

    Admin Controller Rest response

--- Booking WEB/Controllers/AdministratorController.cs
+++ Booking WEB/Controllers/AdministratorController.cs
@@ -1,6 +1,7 @@
 ﻿using Booking_WEB.Data;
 using Booking_WEB.Data.DataAccessors;
 using Booking_WEB.Data.Entities;
+using Booking_WEB.Models.Rest;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,7 +14,7 @@ namespace Booking_WEB.Controllers
         private readonly RealtyAccessor _realtyAccessor = realtyAccessor;
         private readonly UserAccessAccessor _userAccessAccessor = userAccessAccessor;
 
-        public async Task<IActionResult> GetRealtiesTable()
+        public async Task<ActionResult<RestResponse>> GetRealtiesTable()
         {
             var realties = await _realtyAccessor.GetAllAsync(isEditable: false);
             string? tableBodyContent = "";
@@ -22,10 +23,16 @@ namespace Booking_WEB.Controllers
                 tableBodyContent += $"<tr><td>{realty.Name}</td> <td>{realty.Description}</td> " +
                     $"<td>{realty.Slug}</td> <td>{realty.Price}</td> <td>{realty.City.Country.Name}</td> <td>{realty.City.Name}</td> <td>{realty.RealtyGroup.Name}</td> </tr>";
             }
-            return Content(tableBodyContent, "text/html");
+            //return Content(tableBodyContent, "text/html");
+            return new RestResponse()
+            {
+                Status = RestStatus.RestStatus200,
+                Meta = new RestMeta(),
+                Data = tableBodyContent
+            };
         }
 
-        public async Task<IActionResult> GetUsersTable()
+        public async Task<ActionResult<RestResponse>> GetUsersTable()
         {
             //var userAccesses = (from userAccess in _dataContext.UserAccesses 
             //            join user in _dataContext.Users on userAccess.UserId equals user.Id 
@@ -40,7 +47,14 @@ namespace Booking_WEB.Controllers
                     $"<td>{userAccess.UserData.Email}</td> <td>{userAccess.Login}</td> <td>{userAccess.UserData.BirthDate}</td> <td>{userAccess.UserRole.Id}</td></tr>";
             }
 
-            return Content(tableBodyContent, "text/html");
+            //return Content(tableBodyContent, "text/html");
+            //return Json(new {Content = tableBodyContent });
+            return new RestResponse()
+            {
+                Status = RestStatus.RestStatus200,
+                Meta = new RestMeta(),
+                Data = tableBodyContent
+            };
         }
     }
 }
