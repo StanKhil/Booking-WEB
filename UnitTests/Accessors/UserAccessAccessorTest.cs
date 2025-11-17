d2dafe0dfd2e4060d929540cf26d98a4d0ee7627
Stanislav <stankhil@gmail.com>
2025-11-17 15:38:00 +0100

    first tests

---
+++ UnitTests/Accessors/UserAccessAccessorTest.cs
@@ -0,0 +1,127 @@
+﻿using Booking_WEB.Data;
+using Booking_WEB.Data.DataAccessors;
+using Booking_WEB.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitTests.Accessors
+{
+    [TestClass]
+    public class UserAccessAccessorTests
+    {
+        private DataContext GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            return new DataContext(options);
+        }
+
+        private UserAccess CreateTestUser(string login = "testuser")
+        {
+            return new UserAccess
+            {
+                Id = Guid.NewGuid(),
+                Login = login,
+                Dk = "derived key",
+                Salt = "salt",
+                UserData = new UserData
+                {
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Email = "john.doe@test.com",
+                    BirthDate = new DateTime(1990, 1, 1)
+                },
+                UserRole = new UserRole { Id = "SelfRegistered", Description = "!" }
+            };
+        }
+
+        [TestMethod]
+        public async Task LoginExistsAsync_ShouldReturnTrueIfLoginExists()
+        {
+            var context = GetDbContext();
+            var accessor = new UserAccessAccessor(context, NullLogger<UserAccessAccessor>.Instance);
+
+            var user = CreateTestUser();
+            context.UserAccesses.Add(user);
+            await context.SaveChangesAsync();
+
+            var exists = await accessor.LoginExistsAsync(user.Login);
+
+            Assert.IsTrue(exists);
+        }
+
+        [TestMethod]
+        public async Task CreateAsync_ShouldAddUserAccess()
+        {
+            var context = GetDbContext();
+            var accessor = new UserAccessAccessor(context, NullLogger<UserAccessAccessor>.Instance);
+
+            var user = CreateTestUser();
+
+            var result = await accessor.CreateAsync(user);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(user.Login, result.Login);
+            Assert.AreEqual(1, context.UserAccesses.Count());
+        }
+
+        [TestMethod]
+        public async Task DeleteUserAsync_ShouldMarkUserAsDeleted()
+        {
+            var context = GetDbContext();
+            var accessor = new UserAccessAccessor(context, NullLogger<UserAccessAccessor>.Instance);
+
+            var user = CreateTestUser();
+            context.UserAccesses.Add(user);
+            await context.SaveChangesAsync();
+
+            var deleted = await accessor.DeleteUserAsync(user.Login);
+
+            Assert.IsTrue(deleted);
+            var updatedUser = await context.UserAccesses.Include(u => u.UserData)
+                .FirstAsync(u => u.Login == user.Login);
+            Assert.AreEqual("", updatedUser.UserData.FirstName);
+            Assert.AreEqual("", updatedUser.UserData.LastName);
+            Assert.AreEqual("", updatedUser.UserData.Email);
+            Assert.IsNotNull(updatedUser.UserData.DeletedAt);
+        }
+
+        [TestMethod]
+        public async Task GerUserAccessByLoginAsync_ShouldReturnUser_WhenNotDeleted()
+        {
+            var context = GetDbContext();
+            var accessor = new UserAccessAccessor(context, NullLogger<UserAccessAccessor>.Instance);
+
+            var user = CreateTestUser();
+            context.UserAccesses.Add(user);
+            await context.SaveChangesAsync();
+
+            var result = await accessor.GerUserAccessByLoginAsync(user.Login);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(user.Login, result.Login);
+        }
+
+        [TestMethod]
+        public async Task GetByIdAsync_ShouldReturnUserById()
+        {
+            var context = GetDbContext();
+            var accessor = new UserAccessAccessor(context, NullLogger<UserAccessAccessor>.Instance);
+
+            var user = CreateTestUser();
+            context.UserAccesses.Add(user);
+            await context.SaveChangesAsync();
+
+            var result = await accessor.GetByIdAsync(user.Id);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(user.Id, result.Id);
+        }
+    }
+}
---
+++ UnitTests/Accessors/UserDataAccessorTest.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Booking_WEB.Data.DataAccessors;
+using Booking_WEB.Data;
+using Booking_WEB.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Accessors
+{
+    [TestClass]
+    public class UserDataAccessorTest
+    {
+        private DataContext GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            return new DataContext(options);
+        }
+
+        private UserData CreateValidUser(Guid? id = null)
+        {
+            return new UserData
+            {
+                Id = id ?? Guid.NewGuid(),
+                FirstName = "Test",
+                LastName = "User",
+                Email = "test@example.com",
+            };
+        }
+
+        private Cards CreateValidCard(Guid? userId = null)
+        {
+            return new Cards
+            {
+                Id = Guid.NewGuid(),
+                Number = "123456",
+                CardholderName = "Test User",
+                UserId = userId ?? Guid.NewGuid()
+            };
+        }
+
+        // ------------------ TESTS ---------------------
+
+        [TestMethod]
+        public async Task CreateAsync_ShouldCreateUser()
+        {
+            var context = GetDbContext();
+            var accessor = new UserDataAccessor(context);
+
+            var user = CreateValidUser();
+
+            await accessor.CreateAsync(user);
+
+            var saved = await context.Users.FirstOrDefaultAsync();
+            Assert.IsNotNull(saved);
+            Assert.AreEqual("Test", saved!.FirstName);
+        }
+
+        [TestMethod]
+        public async Task UpdateAsync_ShouldUpdateUser()
+        {
+            var context = GetDbContext();
+            var accessor = new UserDataAccessor(context);
+
+            var user = CreateValidUser();
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            user.FirstName = "New Name";
+            await accessor.UpdateAsync(user);
+
+            Assert.AreEqual("New Name", context.Users.First().FirstName);
+        }
+
+        [TestMethod]
+        public async Task ExistsAsync_ShouldReturnTrue_WhenUserExists()
+        {
+            var context = GetDbContext();
+            var accessor = new UserDataAccessor(context);
+
+            var id = Guid.NewGuid();
+            var user = CreateValidUser(id);
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            var exists = await accessor.ExistsAsync(id);
+
+            Assert.IsTrue(exists);
+        }
+
+        [TestMethod]
+        public async Task ExistsAsync_ShouldReturnFalse_WhenUserNotExists()
+        {
+            var context = GetDbContext();
+            var accessor = new UserDataAccessor(context);
+
+            var exists = await accessor.ExistsAsync(Guid.NewGuid());
+
+            Assert.IsFalse(exists);
+        }
+
+        [TestMethod]
+        public async Task GetByIdAsync_ShouldReturnUserWithIncludes()
+        {
+            var context = GetDbContext();
+            var accessor = new UserDataAccessor(context);
+
+            var id = Guid.NewGuid();
+            var user = CreateValidUser(id);
+
+            user.UserAccesses = new List<UserAccess>
+            {
+                new UserAccess { Id = Guid.NewGuid(), Dk = "123", Login = ("testLogin" + id.ToString()), Salt = "salt", RoleId = "SelfRegistered" 
+                }
+            };
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            var result = await accessor.GetByIdAsync(id);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result!.UserAccesses.Count);
+        }
+
+        [TestMethod]
+        public async Task CreateCardAsync_ShouldAddCard()
+        {
+            var context = GetDbContext();
+            var accessor = new UserDataAccessor(context);
+
+            var card = CreateValidCard();
+
+            await accessor.CreateCardAsync(card);
+
+            var saved = await context.Cards.FirstOrDefaultAsync();
+            Assert.IsNotNull(saved);
+            Assert.AreEqual("123456", saved!.Number);
+        }
+
+        [TestMethod]
+        public async Task GetCardsByUserIdAsync_ShouldReturnAllUserCards()
+        {
+            var context = GetDbContext();
+            var accessor = new UserDataAccessor(context);
+
+            var userId = Guid.NewGuid();
+
+            context.Cards.Add(CreateValidCard(userId));
+            context.Cards.Add(CreateValidCard(userId));
+            context.Cards.Add(CreateValidCard(Guid.NewGuid()));
+
+            await context.SaveChangesAsync();
+
+            var cards = await accessor.GetCardsByUserIdAsync(userId);
+
+            Assert.AreEqual(0, cards.Count);
+        }
+    }
+}
